990aedb83fad4d49f552a8e32ab81206d8585cff
李洪智 <123456789>
2018-05-16 10:18:22 +0800

    修订

--- src/test/java/com/util/ExcelUtilTEst.java
+++ src/test/java/com/util/ExcelUtilTEst.java
@@ -1,5 +1,7 @@
 package com.util;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.bean.Person;
 import org.junit.Test;
 
@@ -11,15 +13,20 @@ public class ExcelUtilTEst {
     public void readExcel(){
         ArrayList list =  ExcelUtil.readExcel(new File("E:\\idea\\UtilsClass\\src\\main\\testfile\\test.xls"));
         System.out.println(list);
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        Person person = null;
         for(int i = 0; i < list.size(); i++){
             ArrayList list1 = (ArrayList) list.get(i);
 //            System.out.println(list1.get(0));
-            Person person = new Person();
+            person = new Person();
             person.setName((String) list1.get(0));
             person.setSex((String) list1.get(1));
             person.setEducation((String) list1.get(3));
             person.setAddress((String) list1.get(2));
-            System.out.println(person.toString());
+            jsonArray.add(person);
+//            System.out.println(person.toString());
         }
+        System.out.println(jsonArray.toJSONString());
     }
 }
